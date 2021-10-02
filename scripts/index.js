fd80a071f9ee6a5c12edb4bca4e208cd78dd611b
Alain-sys <74666416+Alain-sys@users.noreply.github.com>
2021-10-02 23:16:16 +0200

    fix: theme and localstorage

--- scripts/index.js
+++ scripts/index.js
@@ -3,59 +3,54 @@ let radio2 = document.getElementById('radio2');
 let radio3 = document.getElementById('radio3');
 let radioControl = document.querySelector('.radio-control');
 
-function themeStorage() {
-  let storedValue1 = localStorage.getItem('theme1');
-  let storedValue2 = localStorage.getItem('theme2');
-  let storedValue3 = localStorage.getItem('theme3');
-  let storeControl = localStorage.getItem('cursor');
+let themeId = 0;
+let themeSwitcher = document.querySelectorAll('.theme-switcher');
 
-  if (storedValue1 !== null) {
-    console.log('1 background color etc...');
-    theme();
-  } else if (storedValue2 !== null) {
-    console.log('2 background color etc...');
-    theme();
-  } else if (storedValue3 !== null) {
-    console.log('3 background color etc...');
-    theme();
-  }
-
-  if (storeControl !== null) {
-    theme();
+function getLocalStorage() {
+  for (let i = 0; i < themeSwitcher.length; i++) {
+    let storageThemeSwitcher = themeSwitcher[i];
+    if (storageThemeSwitcher.checked) {
+      console.log('find');
+      let storageThemeSwitcherId = storageThemeSwitcher.id;
+      console.log(storageThemeSwitcherId);
+      if (storageThemeSwitcherId === 'radio1') {
+        localStorage.setItem('switchTheme', storageThemeSwitcherId);
+        // radioControl.style.left = '6%';
+      } else if (storageThemeSwitcherId === 'radio2') {
+        localStorage.setItem('switchTheme', storageThemeSwitcherId);
+        // radioControl.style.left = '40%';
+      } else if (storageThemeSwitcherId === 'radio3') {
+        localStorage.setItem('switchTheme', storageThemeSwitcherId);
+        // radioControl.style.left = '75%';
+      }
+    }
   }
 }
-themeStorage();
 
-function theme() {
-  if (radio1.checked) {
+if (localStorage.getItem('switchTheme') == null) {
+  // console.log('localstorage null');
+  getLocalStorage();
+}
+
+function switchTheme() {
+  if (localStorage.getItem('switchTheme') === 'radio1') {
     radioControl.style.left = '6%';
-    localStorage.setItem('theme1', radio1.checked);
-    localStorage.setItem('cursor', radioControl.style.left);
-    localStorage.removeItem('theme2');
-    localStorage.removeItem('theme3');
-  } else if (radio2.checked) {
+    radio1.setAttribute('checked', true);
+  } else if (localStorage.getItem('switchTheme') === 'radio2') {
     radioControl.style.left = '40%';
-    localStorage.setItem('theme2', radio2.checked);
-    localStorage.setItem('cursor', radioControl.style.left);
-    localStorage.removeItem('theme1');
-    localStorage.removeItem('theme3');
-  } else if (radio3.checked) {
+    radio2.setAttribute('checked', true);
+  } else if (localStorage.getItem('switchTheme') === 'radio3') {
     radioControl.style.left = '75%';
-    localStorage.setItem('theme3', radio3.checked);
-    localStorage.setItem('cursor', radioControl.style.left);
-    localStorage.removeItem('theme1');
-    localStorage.removeItem('theme2');
+    radio3.setAttribute('checked', true);
   }
 }
 
-radio1.addEventListener('change', () => {
-  theme();
-});
+switchTheme();
 
-radio2.addEventListener('change', () => {
-  theme();
-});
-
-radio3.addEventListener('change', () => {
-  theme();
+themeSwitcher.forEach((theme) => {
+  theme.addEventListener('change', () => {
+    themeId = theme.id;
+    localStorage.setItem('switchTheme', themeId);
+    switchTheme();
+  });
 });
